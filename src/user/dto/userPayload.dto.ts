910509f1116cddc3f30417f22df943408c113d81
오송주 <dhthdwn7920@gmail.com>
2023-11-20 16:56:28 +0900

    👀 :: userPayload 추가

--- src/user/dto/userPayload.dto.ts
+++ src/user/dto/userPayload.dto.ts
@@ -1,6 +1,9 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UserPayload {
   @IsNumber()
   userID: number;
+
+  @IsString()
+  role: string
 }
