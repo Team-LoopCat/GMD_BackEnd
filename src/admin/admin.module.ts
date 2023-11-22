5ff563ea9022d6362b380224ace2ccd5c3b5fe83
오송주 <dhthdwn7920@gmail.com>
2023-11-23 08:38:07 +0900

    👀 :: fix imports and providers

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Divice } from './entities/divice.entity';
+import { DiviceStatus } from './entities/diviceStatus.entity';
+import { Student } from './entities/student.entity';
+import { UserService } from 'src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Student, DiviceStatus, Divice, User]),
+    JwtModule.register({
+      secret: process.env.SECRET,
+      signOptions: {
+        expiresIn: '4h',
+      },
+      verifyOptions: {
+        complete: false,
+      },
+    }),
+  ],
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [UserService, AdminService],
 })
 export class AdminModule {}
